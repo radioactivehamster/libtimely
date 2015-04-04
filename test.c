6697ac479b6a817058b4fcd1f92ba06e9667cf21
James Benner <james.benner@gmail.com>
2015-04-03 20:15:01 -0400

    Updated the todo list.

--- test.c
+++ test.c
@@ -4,7 +4,23 @@
 #include "_utils.h"
 
 //!
-//! @todo Break down the tests for epic(ish) focus. DO IT ME!!!!!
+//! @todo Generate a broken-down time `tm` struct.
+//!
+//! ### `struct tm` Members
+//!
+//! ```c
+//! int tm_sec;     /* seconds (0 - 60) */
+//! int tm_min;     /* minutes (0 - 59) */
+//! int tm_hour;    /* hours (0 - 23) */
+//! int tm_mday;    /* day of month (1 - 31) */
+//! int tm_mon;     /* month of year (0 - 11) */
+//! int tm_year;    /* year - 1900 */
+//! int tm_wday;    /* day of week (Sunday = 0) */
+//! int tm_yday;    /* day of year (0 - 365) */
+//! int tm_isdst;   /* is summer time in effect? */
+//! char *tm_zone;  /* [non-standard] abbreviation of timezone name */
+//! long tm_gmtoff; /* [non-standard] offset from UTC in seconds */
+//! ```
 //!
 int main(void)
 {
