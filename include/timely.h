6151f028ff0d245012aa9cd7e2b494cef3d00aea
James Benner <james.benner@gmail.com>
2015-04-08 16:00:17 -0400

    Modified comment formatting.

--- include/timely.h
+++ include/timely.h
@@ -1,7 +1,7 @@
 #ifndef TIMELY_H
 #define TIMELY_H
 
-// Time base values for `timespec_get` as per the C11 spec
+//! Time base values for `timespec_get` as per the C11 spec
 #ifndef TIME_UTC
     #define TIME_UTC (1)
 #endif
@@ -15,7 +15,5 @@
 #include "timely/rfc1123_gmt_timestamp.h"
 #include "timely/set_default_locale.h"
 #include "timely/tm_ctor.h"
-
-//-/ --------------------
 
 #endif  /* TIMELY_H */
